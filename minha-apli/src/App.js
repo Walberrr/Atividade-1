00007104c6fcbc80773fd2e87c94fc22c564fdc5
Walberrr <116041394+Walberrr@users.noreply.github.com>
2024-08-13 19:31:47 +0000

    foi

---
+++ minha-apli/src/App.js
@@ -0,0 +1,18 @@
+import Componente1 from './componente1';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Componente1 />
+      <hr />
+      <Componente1 />
+      <hr />
+      <Componente1 />
+      <br/>
+      <br/>
+    </div>
+  );
+};
+
+export default App;
+
---
+++ minha-apli/src/componente1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './componente1.css'; 
+
+const Componente1 = () => {
+  return (
+    <div className="container">
+      <img className="centralizar-imagem" src="https://th.bing.com/th/id/R.3fb35e974fc83083132297810b418ada?rik=qMLWmbBYDv8rlQ&pid=ImgRaw&r=0" alt="Logo da UNICAP" />
+      <p>A Universidade Católica de Pernambuco (UNICAP), popularmente conhecida apenas como a Católica, é uma instituição de ensino superior privada, filantrópica e confessional, localizada em Recife. Criada em 27 de setembro de 1951 e reconhecida pelo Governo Federal por meio do Decreto 30.417 de 18 de Janeiro de 1952.</p>
+    </div>
+  );
+};
+
+export default Componente1;
+
+
+
